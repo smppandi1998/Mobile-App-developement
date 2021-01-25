ba66fb8621a4446d1b1ae1cbc0a31e35116f3ec6
smppandi1998 <muthupandip1998@gmail.com>
2021-01-25 19:49:29 +0530

    weradiate template upload

---
+++ Weradiat1/src/screens/StartScreen.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+
+const StartScreen = ({ navigation }) => (
+  <Background>
+    <Logo />
+    <Header>DNC</Header>
+    <Paragraph>
+    
+    </Paragraph>
+    <Button mode="contained" onPress={() => navigation.navigate('LoginScreen')}>
+      Login
+    </Button>
+    <Button
+      mode="outlined"
+      onPress={() => navigation.navigate('UserScreen')}
+    >
+      Sign Up
+    </Button>
+  </Background>
+)
+
+export default StartScreen
